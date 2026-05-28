513235a3359d291c8cf476bbf072f0a03f3b4dd1
Ncode <94313713+NicolasGounotEsiea@users.noreply.github.com>
2026-05-28 14:34:39 +0200

    Refactor Ping message handling for clarity

--- src-tauri/src/sharing/client.rs
+++ src-tauri/src/sharing/client.rs
@@ -125,8 +125,11 @@ pub async fn connect(
                                 }
                             }
                         }
-                        Some(Ok(Message::Ping(data)))
-                            if sink.send(Message::Pong(data)).await.is_err() => { break; }
+                        Some(Ok(Message::Ping(data))) => {
+                            if sink.send(Message::Pong(data)).await.is_err() {
+                                break;
+                            }
+                        }
                         Some(Ok(Message::Close(_))) | None => break,
                         _ => {}
                     }
